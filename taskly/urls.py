3d5b5638bacb9a800ec52c784df0d135ed74802f
Kai Garrod <kaigarrod@gmail.com>
2025-03-26 22:35:57 +0000

    Add Django Debug Toolbar

--- taskly/urls.py
+++ taskly/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +24,4 @@ urlpatterns = [
     re_path(r'^auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.jwt')),
 
-]
+] + debug_toolbar_urls()
