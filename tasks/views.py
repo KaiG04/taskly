98d3d122098cbd64e138798699090eefb5626e04
Kai Garrod <kaigarrod@gmail.com>
2025-03-26 23:28:20 +0000

    Add Filter, Search and Ordering

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Task, Category
 from .serializers import TaskSerializer, CategorySerializer
@@ -10,8 +12,16 @@ from .serializers import TaskSerializer, CategorySerializer
 class TaskViewSet(ModelViewSet):
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['category']
+    search_fields = ['title']
+    ordering_fields = ['created_at']
+
+
 
     def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Task.objects.all()
         return Task.objects.select_related('category').filter(user=self.request.user)
 
 
