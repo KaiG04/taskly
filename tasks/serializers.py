87793816a8c920ba5fa70a7d16d6b5dea340ea04
Kai Garrod <kaigarrod@gmail.com>
2025-04-01 22:49:34 +0100

    Update TaskCardSerializer to include tasks when calling endpoint /cards/{uuid}/ and add some more read_only fields

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -2,6 +2,7 @@ from django.core.exceptions import ValidationError as DjangoValidationError
 
 from rest_framework import serializers
 
+
 from .models import TaskCard, Task
 from .validators import validate_deadline
 
@@ -16,20 +17,22 @@ class DeadlineValidationMixin:
             raise serializers.ValidationError(e.messages) # Convert Django error to DRF error
         return value
 
-class TaskCardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TaskCard
-        fields = ['id', 'slug', 'title', 'visibility', 'created_at', 'last_updated', 'owner']
-        read_only_fields = ['slug', 'created_at', 'last_updated', 'owner']
-
-
-
 class TaskSerializer(serializers.ModelSerializer, DeadlineValidationMixin):
     class Meta:
         model = Task
-        fields = ['id', 'title', 'description', 'created_at', 'created_by', 'priority', 'deadline',
+        fields = ['slug', 'title', 'description', 'created_at', 'created_by', 'priority', 'deadline',
                   'completed']
-        read_only_fields = ['created_by']
+        read_only_fields = ['created_by', 'slug']
+
+
+class TaskCardSerializer(serializers.ModelSerializer):
+    tasks = TaskSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = TaskCard
+        fields = ['id', 'slug', 'title', 'visibility', 'created_at', 'last_updated', 'owner', 'tasks']
+        read_only_fields = ['slug', 'created_at', 'last_updated', 'owner']
+
 
 
 class TaskUpdateSerializer(serializers.ModelSerializer, DeadlineValidationMixin):
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
 from django.urls.conf import include
+
+from .views import TaskCardViewSet, TaskViewSet
+
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 
 from . import views
 
 router = DefaultRouter()
-router.register('cards', views.TaskCardViewSet, basename='cards')
+router.register(r'cards', TaskCardViewSet, basename='cards') #cards-list, cards-detail
+
+cards_router = routers.NestedDefaultRouter(router, r'cards', lookup='card')
+cards_router.register('tasks', TaskViewSet, basename='card-tasks')
+
 
 # URLConf
-urlpatterns = router.urls
+urlpatterns = router.urls + cards_router.urls
