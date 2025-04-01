1a485c862f045c6c94eb92c14f086a14370c57a7
Kai Garrod <kaigarrod@gmail.com>
2025-04-01 22:48:52 +0100

    Update perform_create on TaskViewSet in order to save slug to task.

--- tasks/views.py
+++ tasks/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import SAFE_METHODS
 from django_filters.rest_framework import DjangoFilterBackend
+from django.utils.text import slugify
 
 from .models import Task, TaskCard
 from .permissions import IsOwnerOrReadOnly
@@ -14,7 +15,8 @@ from .serializers import TaskSerializer, TaskUpdateSerializer, TaskCardSerialize
 class TaskCardViewSet(ModelViewSet):
     queryset = TaskCard.objects.all()
     serializer_class = TaskCardSerializer
-    lookup_field = 'slug'
+    lookup_field = "slug"
+
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -23,10 +25,10 @@ class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all()
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    lookup_field = 'slug'
     search_fields = ['title']
     ordering_fields = ['created_at']
     http_method_names = ['get', 'post', 'put', 'patch', 'delete']
-
 
     def get_serializer_class(self):
         if self.request.method in SAFE_METHODS or self.request.method == 'POST':
@@ -35,7 +37,12 @@ class TaskViewSet(ModelViewSet):
             return TaskUpdateSerializer
 
 
+
     def perform_create(self, serializer):
-        serializer.save(created_by=self.request.user)
+        task_card = TaskCard.objects.get(slug=self.kwargs['card_slug'])
+        task = serializer.save(created_by=self.request.user, task_card=task_card)
+
+        task.slug = slugify(f"{task.id}-{task.title}")
+        task.save()
 
 
