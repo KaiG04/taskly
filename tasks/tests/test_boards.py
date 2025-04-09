338af5ee527d12991c89c5e0755b4efd896abcb0
Kai Garrod <kaigarrod@gmail.com>
2025-04-09 20:18:10 +0100

    Split up tests into Get list & Get instance for better readability, move both under one class 'TestGetTaskBoards' for better layout / readability

--- tasks/tests/test_boards.py
+++ tasks/tests/test_boards.py
@@ -7,72 +7,73 @@ from tasks.models import TaskBoard
 
 
 @pytest.mark.django_db
-class TestGetBoard:
-    def test_if_user_is_anonymous_return_401(self, user, api_client):
-        """
-        Test that the user is authenticated in order to access the task board.
-        :param user:
-        :param api_client:
-        """
-        response = api_client.get(f'/{user.username}/boards/')
+class TestGetTaskBoards:
 
-        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    class TestGetTaskBoardList:
+        def test_if_user_is_anonymous_return_401(self, user, api_client):
+            """
+            Test that the user is authenticated in order to access the task board.
+            :param user:
+            :param api_client:
+            """
+            response = api_client.get(f'/{user.username}/boards/')
 
-    def test_if_user_is_authorised_return_200(self, user, api_client):
-        """
-        Test that the user is authenticated in order to access the task board.
-        :param user:
-        :param api_client:
-        """
-        api_client.force_authenticate(user=user)
+            assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-        response = api_client.get(f'/{user.username}/boards/')
+        def test_if_user_is_authorised_return_200(self, user, api_client):
+            """
+            Test that the user is authenticated in order to access the task board.
+            :param user:
+            :param api_client:
+            """
+            api_client.force_authenticate(user=user)
 
-        assert response.status_code == status.HTTP_200_OK
+            response = api_client.get(f'/{user.username}/boards/')
 
-    def test_if_user_is_authenticated_and_if_changes_url_gets_redirected_returns_302(self, user, api_client):
-        """
-        Test that if the user is authenticated, and trys to access another task board by changing the url gets
-        redirected to their task board and receives 302.
-        :param user:
-        :param api_client:
-        """
-        api_client.force_authenticate(user=user)
-        target_username = "bob"
+            assert response.status_code == status.HTTP_200_OK
 
-        response = api_client.get(f'/{target_username}/boards/')
+        def test_if_user_is_authenticated_and_if_changes_url_gets_redirected_returns_302(self, user, api_client):
+            """
+            Test that if the user is authenticated, and trys to access another task board by changing the url gets
+            redirected to their task board and receives 302.
+            :param user:
+            :param api_client:
+            """
+            api_client.force_authenticate(user=user)
+            target_username = "bob"
 
-        assert response.status_code == status.HTTP_302_FOUND
+            response = api_client.get(f'/{target_username}/boards/')
 
-    def test_if_user_is_authenticated_can_retrieve_task_board_detail_returns_200(self,
-                                                                                 user, api_client, valid_board_data):
-        """
-        Test that the user is authenticated, and can retrieve task board detail.
-        :param user:
-        :param api_client:
-        :param valid_board_data:
-        """
-        api_client.force_authenticate(user=user)
-        api_client.post(f'/{valid_board_data["owner"]}/boards/', data=valid_board_data)
+            assert response.status_code == status.HTTP_302_FOUND
 
-        response = api_client.get(f'/{valid_board_data["owner"]}/boards/{valid_board_data["slug"]}/')
+    class TestGetTaskBoardInstance:
+        def test_if_user_is_anonymous_cannot_retrieve_task_board_instance_returns_401(self
+                                                                                 , user, api_client, valid_board_data):
+            """
+            Test that the user is anonymous, and cannot retrieve task board instance.
+            :param user:
+            :param api_client:
+            :param valid_board_data:
+            """
+            api_client.post(f'/{valid_board_data["owner"]}/boards/', data=valid_board_data)
 
-        assert response.status_code == status.HTTP_200_OK
+            response = api_client.get(f'/{valid_board_data["owner"]}/boards/{valid_board_data["slug"]}/')
 
-    def test_if_user_is_anonymous_cannot_retrieve_task_board_detail_returns_401(self
-                                                                             , user, api_client, valid_board_data):
-        """
-        Test that the user is anonymous, and cannot retrieve task board detail.
-        :param user:
-        :param api_client:
-        :param valid_board_data:
-        """
-        api_client.post(f'/{valid_board_data["owner"]}/boards/', data=valid_board_data)
+            assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-        response = api_client.get(f'/{valid_board_data["owner"]}/boards/{valid_board_data["slug"]}/')
+        def test_if_user_is_authenticated_can_retrieve_task_board_instance_returns_200(self,
+                                                                                     user, api_client, valid_board_data):
+            """
+            Test that the user is authenticated, and can retrieve task board instance.
+            :param user:
+            :param api_client:
+            :param valid_board_data:
+            """
+            api_client.force_authenticate(user=user)
+            api_client.post(f'/{valid_board_data["owner"]}/boards/', data=valid_board_data)
 
-        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+            response = api_client.get(f'/{valid_board_data["owner"]}/boards/{valid_board_data["slug"]}/')
 
-
+            assert response.status_code == status.HTTP_200_OK
 
 
